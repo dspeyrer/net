3657e372ae86fc4b6d3a43f121897dbfb39342c5
Deven Speyrer <devenspeyrer@gmail.com>
2025-03-24 15:46:28 -0600

    Remove Cursor::rsplit_n

--- collections/src/bytes/cursor.rs
+++ collections/src/bytes/cursor.rs
@@ -66,14 +66,6 @@ impl<'a> Cursor<'a> {
 		(bytes::as_slice_mut(l), Self { slice: r, pivot: self.pivot })
 	}
 
-	/// Splits off an instance of a type after the pivot point, returning the preceding buffer, and advancing the pivot to the end of the current buffer.
-	#[inline]
-	pub fn rsplit_n<T: Cast>(self, n: usize) -> (Self, &'a mut [T]) {
-		let (l, r) = self.slice.split_at_mut(self.pivot());
-		*self.pivot = r.as_ptr() as usize + size_of::<T>();
-		(Cursor { slice: l, pivot: self.pivot }, bytes::as_slice_mut(r))
-	}
-
 	/// Returns a cursor limited to `len` bytes.
 	#[inline]
 	pub fn lim(self, len: usize) -> Self {
